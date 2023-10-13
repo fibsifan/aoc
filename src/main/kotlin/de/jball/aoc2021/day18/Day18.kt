2a450b734acbe3c677179d055fcf6d754984c441
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-10-13 03:02:46 +0200

    start 2021.day18

--- src/main/kotlin/de/jball/aoc2021/day18/Day18.kt
+++ src/main/kotlin/de/jball/aoc2021/day18/Day18.kt
@@ -1,24 +1,124 @@
 package de.jball.aoc2021.day18
 
 import de.jball.aoc2021.Day
+import java.util.*
+import kotlin.math.max
 
 class Day18(test: Boolean = false): Day(test, expected1 = 4140) {
+    private val sailfishNumbers = input.map { line -> SailfishNumber.fromString(line) }
+
     override fun part1(): Long {
+        println(sailfishNumbers.joinToString("\n"))
         TODO("Not yet implemented")
     }
 
     override fun part2(): Long {
         TODO("Not yet implemented")
     }
+
+
 }
 
-private data class SnailfishNumber(val first: Any, val second: Any) {
-    operator fun plus(increment: SnailfishNumber): SnailfishNumber {
-        return SnailfishNumber(this, increment).reduced()
+sealed class SailfishNumberComponent {
+    abstract fun magnitude(): Int
+    abstract fun height(): Int
+    abstract fun depth(): Int
+}
+
+private class SailfishInt(val delegate: Int): SailfishNumberComponent() {
+    override fun magnitude(): Int {
+        return delegate
     }
 
-    private fun reduced(): SnailfishNumber {
+    override fun height(): Int {
+        return 0
+    }
+
+    override fun depth(): Int {
         TODO("Not yet implemented")
+    }
+
+    override fun toString(): String {
+        return delegate.toString()
+    }
+}
+
+class SailfishNumber(
+    a: SailfishNumberComponent,
+    b: SailfishNumberComponent
+): SailfishNumberComponent() {
+    companion object {
+        fun fromString(sailfishNumberString: String): SailfishNumber {
+            var workingString = sailfishNumberString
+            val stack: Deque<SailfishNumberComponent> = LinkedList()
+            while (workingString.isNotEmpty()) {
+                if (workingString.startsWith("[")) {
+                    // do nothing
+                } else if (workingString.startsWith("]")) {
+                    val b = stack.pop()
+                    val a = stack.pop()
+                    stack.push(SailfishNumber(a, b))
+                } else if (workingString.startsWith(",")) {
+                    // do nothing
+                } else {
+                    stack.push(SailfishInt(workingString[0].toString().toInt()))
+                }
+                workingString = workingString.substring(1)
+            }
+
+            if (stack.size != 1) {
+                TODO()
+            }
+
+            return stack.pop() as SailfishNumber
+        }
+    }
+
+    private var delegate = Pair(a, b)
+    private var parent: SailfishNumber? = null
+    init {
+        if (delegate.first is SailfishNumber) {
+            (delegate.first as SailfishNumber).parent = this
+        }
+        if (delegate.second is SailfishNumber) {
+            (delegate.second as SailfishNumber).parent = this
+        }
+    }
+
+    override fun magnitude(): Int {
+        return delegate.first.magnitude()*3 + delegate.second.magnitude()*2
+    }
+
+    override fun height(): Int {
+        return max(delegate.first.height(), delegate.second.height()) + 1
+    }
+
+    override fun depth(): Int {
+        val parentCopy = parent
+        return if (parentCopy == null) {
+            0
+        } else {
+            parentCopy.depth() + 1
+        }
+    }
+
+    override fun toString(): String {
+        return "[${delegate.first},${delegate.second}]"
+    }
+
+    fun component1(): SailfishNumberComponent {
+        return delegate.first
+    }
+    fun component2(): SailfishNumberComponent {
+        return delegate.second
+    }
+
+    operator fun plus(increment: SailfishNumber): SailfishNumber {
+        return SailfishNumber(this, increment).reduced()
+    }
+
+    private fun reduced(): SailfishNumber {
+        return this
     }
 }
 
---
+++ src/test/kotlin/de/jball/aoc2021/day18/SailfishNumberTest.kt
@@ -0,0 +1,27 @@
+package de.jball.aoc2021.day18
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class SailfishNumberTest {
+    @Test
+    fun plusWithoutReduce() {
+        val sn1 = SailfishNumber.fromString("[1,2]")
+        val sn2 = SailfishNumber.fromString("[3,4]")
+
+        val result = sn1 + sn2
+
+        assertEquals("[[1,2],[3,4]]", result.toString())
+    }
+
+    @Test
+    fun plusWithReduce() {
+        val sn1 = SailfishNumber.fromString("[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]")
+        val sn2 = SailfishNumber.fromString("[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]")
+
+        val result = sn1 + sn2
+
+        assertEquals("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]", result.toString())
+    }
+}

e13c1f9f4134877d7c59ec13c80fb936d2e84508
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-12-01 07:39:03 +0100

    2023 day 1

---
+++ src/main/kotlin/de/jball/aoc2023/day01/Day01.kt
@@ -0,0 +1,50 @@
+package de.jball.aoc2023.day01
+
+import de.jball.AdventOfCodeDay
+
+class Day01(test: Boolean = false) : AdventOfCodeDay<Int>(test, 142, 281) {
+    private val  digits = mapOf(
+        "1" to 1,
+        "2" to 2,
+        "3" to 3,
+        "4" to 4,
+        "5" to 5,
+        "6" to 6,
+        "7" to 7,
+        "8" to 8,
+        "9" to 9)
+    private val digitStrings = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9)
+    private val digitRegexString = digits.keys.joinToString("|") + "|" + digitStrings.keys.joinToString("|")
+    override fun part1(): Int {
+        return input1.sumOf { line ->
+            val letters = line.chunked(1)
+            val first = letters.firstNotNullOf { it.toIntOrNull() }
+            val last = letters.last { it.toIntOrNull() != null }.toInt()
+            first * 10 + last
+        }
+    }
+
+    override fun part2(): Int {
+        return input2.sumOf {
+            val digitsMatches = Regex("$digitRegexString").findAll(it)
+            val first = digitsMatches.first().value
+            val last = digitsMatches.last().value
+            val firstDigit = digits[first] ?: digitStrings[first]!!
+            val lastDigit = digits[last] ?: digitStrings[last]!!
+            firstDigit * 10 + lastDigit
+        }
+    }
+}
+
+fun main() {
+    Day01(false).run()
+}
---
+++ src/test/kotlin/de/jball/aoc2023/Tests2023.kt
@@ -0,0 +1,11 @@
+package de.jball.aoc2023
+
+import de.jball.aoc2023.day01.Day01
+import kotlin.test.Test
+
+class Tests2023 {
+    @Test
+    fun day01() {
+        Day01(true).run()
+    }
+}

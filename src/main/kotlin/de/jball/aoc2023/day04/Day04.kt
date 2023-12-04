11ce6d0825518b0bd042817d6990678d68db312c
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-12-04 09:31:33 +0100

    2023 day 4

---
+++ src/main/kotlin/de/jball/aoc2023/day04/Day04.kt
@@ -0,0 +1,44 @@
+package de.jball.aoc2023.day04
+
+import de.jball.AdventOfCodeDay
+
+class Day04(test: Boolean = false): AdventOfCodeDay<Int>(test, 13, 30) {
+    override fun part1(): Int {
+        return input1.map {
+            parseGame(it)
+        }.sumOf { game ->
+            val matches = game.first.intersect(game.second)
+            if (matches.isEmpty()) {
+                0
+            } else {
+                (1 shl (matches.size-1))
+            }
+        }
+    }
+
+    private fun parseGame(line: String): Pair<Set<Int>, Set<Int>> {
+        val numbersStrings = line.split(": ")[1].split(" | ")
+        val winningNumbers = numbersStrings[0].chunked(3).map { it.trim().toInt() }.toSet()
+        val drawnNumbers = numbersStrings[1].chunked(3).map { it.trim().toInt() }.toSet()
+        return Pair(winningNumbers, drawnNumbers)
+    }
+
+    override fun part2(): Int {
+        return input2.foldIndexed(MutableList(input2.size) { 1 }) {
+                index: Int, gameCopies: MutableList<Int>, line: String ->
+            val game = parseGame(line)
+            val wins = game.first.intersect(game.second).size
+            if (wins > 0) {
+                val wonCopies = IntRange(index+1, index+wins)
+                wonCopies.forEach { wonCopy ->
+                    gameCopies[wonCopy] += gameCopies[index]
+                }
+            }
+            gameCopies
+        }.sum()
+    }
+}
+
+fun main() {
+    Day04().run()
+}
--- src/test/kotlin/de/jball/aoc2023/Tests2023.kt
+++ src/test/kotlin/de/jball/aoc2023/Tests2023.kt
@@ -2,6 +2,7 @@ package de.jball.aoc2023
 
 import de.jball.aoc2023.day01.Day01
 import de.jball.aoc2023.day02.Day02
+import de.jball.aoc2023.day04.Day04
 import kotlin.test.Test
 
 class Tests2023 {
@@ -13,5 +14,10 @@ class Tests2023 {
     @Test
     fun day02() {
         Day02(true).run()
+    }
+
+    @Test
+    fun day04() {
+        Day04(true).run()
     }
 }

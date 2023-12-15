19970395aff08f92fa02b3214790f0f5970caa42
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2023-12-15 15:14:16 +0100

    2023 day 7 part 2 ugly

--- src/main/kotlin/de/jball/aoc2023/day07/Day07.kt
+++ src/main/kotlin/de/jball/aoc2023/day07/Day07.kt
@@ -2,7 +2,9 @@ package de.jball.aoc2023.day07
 
 import de.jball.AdventOfCodeDay
 
-class Day07(test: Boolean = false): AdventOfCodeDay<Int>(test, 6440, 0) {
+val partTwoCardOrder = "J23456789TQKA"
+
+class Day07(test: Boolean = false) : AdventOfCodeDay<Int>(test, 6440, 5905) {
     private val games = input
         .map { line ->
             val split = line.split(" ")
@@ -20,15 +22,67 @@ class Day07(test: Boolean = false): AdventOfCodeDay<Int>(test, 6440, 0) {
     }
 
     override fun part1(): Int {
-        val gamesInOrderOfHandRank = games.entries.sortedBy { (hand, _) -> hand }
+        val gamesInOrderOfHandRank = games.entries.sortedWith(
+            compareBy({ oneHand, otherHand -> partOneCompareHands(oneHand, otherHand) }) { (hand, _) -> hand })
         return gamesInOrderOfHandRank
-            .foldIndexed(0) { indexOfHand, sum, betsForHand   ->
-                betsForHand.value.sumOf { bet -> (indexOfHand+1) * bet } + sum
+            .foldIndexed(0) { indexOfHand, sum, betsForHand ->
+                betsForHand.value.sumOf { bet -> (indexOfHand + 1) * bet } + sum
             }
     }
 
+
+    private fun partOneCompareHands(one: Hand, other: Hand): Int {
+        return if (one.cardCountsPartOne[0].value.compareTo(other.cardCountsPartOne[0].value) != 0)
+            one.cardCountsPartOne[0].value.compareTo(other.cardCountsPartOne[0].value)
+        else if (one.cardCountsPartOne.size > 1 && one.cardCountsPartOne[1].value.compareTo(other.cardCountsPartOne[1].value) != 0) {
+            one.cardCountsPartOne[1].value.compareTo(other.cardCountsPartOne[1].value)
+        } else if (partOneCompareCardAt(one, other, 0) != 0) {
+            partOneCompareCardAt(one, other, 0)
+        } else if (partOneCompareCardAt(one, other, 1) != 0) {
+            partOneCompareCardAt(one, other, 1)
+        } else if (partOneCompareCardAt(one, other, 2) != 0) {
+            partOneCompareCardAt(one, other, 2)
+        } else if (partOneCompareCardAt(one, other, 3) != 0) {
+            partOneCompareCardAt(one, other, 3)
+        } else if (partOneCompareCardAt(one, other, 4) != 0) {
+            partOneCompareCardAt(one, other, 4)
+        } else {
+            0
+        }
+    }
+
+    private fun partTwoCompareHands(one: Hand, other: Hand): Int {
+        return if (one.cardCountsPartTwo[0].value.compareTo(other.cardCountsPartTwo[0].value) != 0)
+            one.cardCountsPartTwo[0].value.compareTo(other.cardCountsPartTwo[0].value)
+        else if (one.cardCountsPartTwo.size > 1 && one.cardCountsPartTwo[1].value.compareTo(other.cardCountsPartTwo[1].value) != 0) {
+            one.cardCountsPartTwo[1].value.compareTo(other.cardCountsPartTwo[1].value)
+        } else if (partTwoCompareCardAt(one, other, 0) != 0) {
+            partTwoCompareCardAt(one, other, 0)
+        } else if (partTwoCompareCardAt(one, other, 1) != 0) {
+            partTwoCompareCardAt(one, other, 1)
+        } else if (partTwoCompareCardAt(one, other, 2) != 0) {
+            partTwoCompareCardAt(one, other, 2)
+        } else if (partTwoCompareCardAt(one, other, 3) != 0) {
+            partTwoCompareCardAt(one, other, 3)
+        } else if (partTwoCompareCardAt(one, other, 4) != 0) {
+            partTwoCompareCardAt(one, other, 4)
+        } else {
+            0
+        }
+    }
+
+
+    private fun partOneCompareCardAt(one: Hand, other: Hand, index: Int) = one.cards[index].compareTo(other.cards[index])
+    private fun partTwoCompareCardAt(one: Hand, other: Hand, index: Int) = partTwoCardOrder.indexOf(one.cards[index].label)
+        .compareTo(partTwoCardOrder.indexOf(other.cards[index].label))
+
     override fun part2(): Int {
-        TODO("Not yet implemented")
+        val gamesInOrderOfHandRank = games.entries.sortedWith(
+            compareBy({ oneHand, otherHand -> partTwoCompareHands(oneHand, otherHand) }) { (hand, _) -> hand })
+        return gamesInOrderOfHandRank
+            .foldIndexed(0) { indexOfHand, sum, betsForHand ->
+                betsForHand.value.sumOf { bet -> (indexOfHand + 1) * bet } + sum
+            }
     }
 }
 
@@ -48,43 +102,33 @@ enum class Card(val label: String) {
     Ace("A")
 }
 
-data class Hand(val cards: List<Card>) : Comparable<Hand> {
-    private val cardCounts = cards.groupingBy { it }.eachCount().entries
+class Hand(val cards: List<Card>) {
+    val cardCountsPartOne = cards.groupingBy { it }.eachCount().entries
         .sortedWith(compareByDescending<Map.Entry<Card, Int>> { (_, count) -> count }
             .thenByDescending { (card, _) -> card.ordinal })
 
-    override fun compareTo(other: Hand): Int {
-        return if (cardCounts[0].value.compareTo(other.cardCounts[0].value) != 0)
-            cardCounts[0].value.compareTo(other.cardCounts[0].value)
-        else if (cardCounts.size > 1 && cardCounts[1].value.compareTo(other.cardCounts[1].value) != 0) {
-            cardCounts[1].value.compareTo(other.cardCounts[1].value)
-        } else if (compareCardAt(other, 0) != 0) {
-            compareCardAt(other, 0)
-        } else if (compareCardAt(other, 1) != 0) {
-            compareCardAt(other, 1)
-        } else if (compareCardAt(other, 2) != 0) {
-            compareCardAt(other, 2)
-        } else if (compareCardAt(other, 3) != 0) {
-            compareCardAt(other, 3)
-        } else if (compareCardAt(other, 4) != 0) {
-            compareCardAt(other, 4)
-        } else {
-            0
+    val cardCountsPartTwo = groupCardsForPartTwo()
+
+    private fun groupCardsForPartTwo(): List<Map.Entry<Card, Int>> {
+        val groupedCards = cards.groupingBy { it }.eachCount().toMutableMap()
+        val sortedGroupedCards = groupedCards.entries
+            .sortedWith(compareByDescending<Map.Entry<Card, Int>> { (_, count) -> count }
+                .thenByDescending { (card, _) -> partTwoCardOrder.indexOf(card.label) })
+
+        val jackCount = sortedGroupedCards.find { (card, _) -> card == Card.Jack }
+        if (jackCount != null && sortedGroupedCards.size > 1) {
+            val bestCardNotJack = sortedGroupedCards.first { (card, _) -> card != Card.Jack }.key
+            groupedCards[bestCardNotJack] = jackCount.value + groupedCards[bestCardNotJack]!!
+            groupedCards.remove(jackCount.key)
         }
-    }
 
-    private fun compareCardAt(other: Hand, index: Int) = cards[index].compareTo(other.cards[index])
-
-    override fun equals(other: Any?): Boolean {
-        return other is Hand && compareTo(other) == 0
+        return groupedCards.entries
+            .sortedWith(compareByDescending<Map.Entry<Card, Int>> { (_, count) -> count }
+                .thenByDescending { (card, _) -> partTwoCardOrder.indexOf(card.label) })
     }
 
     override fun toString(): String {
-        return cards.joinToString("") { card -> card.label}
-    }
-
-    override fun hashCode(): Int {
-        return cardCounts.hashCode()
+        return cards.joinToString("") { card -> card.label }
     }
 }
 

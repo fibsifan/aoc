6fbe5cf40814a913e9c308eae18b03b1b101c261
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2024-12-03 17:21:08 +0100

    2024 Day 03 part 2

--- src/main/kotlin/de/jball/aoc2024/day03/Day03.kt
+++ src/main/kotlin/de/jball/aoc2024/day03/Day03.kt
@@ -2,18 +2,24 @@ package de.jball.aoc2024.day03
 
 import de.jball.AdventOfCodeDay
 
-class Day03(test: Boolean = false): AdventOfCodeDay<Long>(test, 161, 0) {
-	private val regex1 = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+class Day03(test: Boolean = false): AdventOfCodeDay<Long>(test, 161, 48) {
+	private val multiplicationRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+	private val deactivationRegex = Regex("don't\\(\\).+?do\\(\\)")
+
 	override fun part1(): Long {
 		return input.sumOf { line ->
-			regex1.findAll(line)
+			multiplicationRegex.findAll(line)
 				.map { it.groupValues[1].toLong() * it.groupValues[2].toLong() }
 				.sum()
 		}
 	}
 
 	override fun part2(): Long {
-		TODO("Not yet implemented")
+		val cleanedInput = input.reduce { a, b -> a + b }
+			.replace(deactivationRegex, "")
+		return multiplicationRegex.findAll(cleanedInput)
+			.map { it.groupValues[1].toLong() * it.groupValues[2].toLong() }
+			.sum()
 	}
 }
 

a915e02010c5d1fcb0562719a16c2649085de8a7
Johannes Ballmann <574988+fibsifan@users.noreply.github.com>
2025-02-23 22:00:34 +0100

    2024 Day 6 wip

---
+++ src/main/kotlin/de/jball/aoc2024/day06/Day06.kt
@@ -0,0 +1,13 @@
+package de.jball.aoc2024.day06
+
+import de.jball.AdventOfCodeDay
+
+class Day06(test: Boolean = false): AdventOfCodeDay<Int>(test, 0, 0) {
+	override fun part1(): Int {
+		TODO("Not yet implemented")
+	}
+
+	override fun part2(): Int {
+		TODO("Not yet implemented")
+	}
+}
